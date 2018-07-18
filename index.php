f462aab06f40702fde817ce3d19dfda878dc82e9
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-17 22:02:53 -0500

    validando que se hayan enviado los datos

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
+$errores = '';
+$enviado = '';
+if (isset($_POST['submit'])) {
+  // code...
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
+  $mensaje = $_POST['mensaje'];
+
+  if (!empty($nombre)) {
+    $nombre = trim($nombre);
+    $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+  }
+  else {
+        $errores .= 'Por favor ingresa un nombre <br>';
+        }
+
+  if (!empty($email)) {
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+        $errores .= 'Por favor ingresa un correo valido <br>';
+      }
+
+  }else {
+      $errores .= 'Por favor ingresa un correo <br>';
+  }
+}
+
 
 require 'index.view.php';
+
 
  ?>
--- index.view.php
+++ index.view.php
@@ -13,7 +13,15 @@
         <input type="text" class="form-control"id="nombre" name="nombre" placeholder="nombre: " value="">
           <input type="email" class="form-control"id="correo" name="email" placeholder="correo: " value="">
           <textarea name="mensaje" class="form-control" id="mensaje" placeholder="mensaje" ></textarea>
-
+            <?php if(!empty($errores)): ?>
+                  <div class="alert error">
+                    <?php echo $errores; ?>
+                  </div>
+                <?php elseif($enviado): ?>
+                  <div class="aler exito">
+                    <p>Enviado correctamente</p>
+                  </div>
+              <?php endif ?>
           <input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
 
       </form>
